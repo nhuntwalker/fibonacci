ae26837f1afaf19da7a976abde750ef5c27bb9ec
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-08-11 09:29:25 -0700

    I made my loop. I think I'm done. Still have to check in the browser.

--- fib.js
+++ fib.js
@@ -4,4 +4,18 @@
 0 1 1 2 3 5 8 13 21 34
 */
 
-var fibArray = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+var fibArray = [];
+var firstNum = 0;
+var secondNum = 1;
+
+for (var i = 0; i < 10; i++) {
+  if (i === 0) {
+    fibArray.push(firstNum);
+  } else if (i === 1) {
+    fibArray.push(secondNum);
+  }
+  var tmp = firstNum + secondNum;
+  firstNum = secondNum;
+  secondNum = tmp;
+  fibArray.push(tmp);
+}
