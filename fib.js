b228b9cd71a6647b7ef07787cdcbca4e6d98bef6
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-08-11 11:59:25 -0700

    I made things into a function

--- fib.js
+++ fib.js
@@ -4,20 +4,23 @@
 0 1 1 2 3 5 8 13 21 34
 */
 
-var fibArray = [];
-var firstNum = 0;
-var secondNum = 1;
+function fibonacci(){
+  var fibArray = [];
+  var firstNum = 0;
+  var secondNum = 1;
 
-for (var i = 0; i < 10; i++) {
-  if (i === 0) {
-    fibArray.push(firstNum);
-    continue;
-  } else if (i === 1) {
-    fibArray.push(secondNum);
-    continue;
+  for (var i = 0; i < 10; i++) {
+    if (i === 0) {
+      fibArray.push(firstNum);
+      continue;
+    } else if (i === 1) {
+      fibArray.push(secondNum);
+      continue;
+    }
+    var tmp = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = tmp;
+    fibArray.push(tmp);
   }
-  var tmp = firstNum + secondNum;
-  firstNum = secondNum;
-  secondNum = tmp;
-  fibArray.push(tmp);
 }
+fibonacci();
